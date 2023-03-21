5171cd72399a198f96fae9fd2f88239b73c6383f
Theodoric Graham <theodoricgraham@gmail.com>
2023-03-21 12:26:24 -0500

    finished array string exercises

--- js/planets-string.js
+++ js/planets-string.js
@@ -23,14 +23,16 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-    var planetsStringB = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
+    // var planetsStringB = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
     var planetsStringLi = '<ul><li>Mercury</li> <li>Venus</li> <li>Earth</li> <li>Mars</li> <li>Jupiter</li> <li>Saturn</li> <li>Uranus</li> <li>Neptune</li> </ul>'
-    // planetsArray.forEach(planet => {
-    //     document.getElementById('planets').innerHTML += `<li>${planet}</li>`
-    // })
+    planetsArray.forEach(planet => {
+        document.getElementById('planets').innerHTML += `<li>${planet}</li>`
+    })
     document.getElementById('planets').innerHTML = planetsStringLi
 
     // document.getElementById('planets').innerHTML = planetsStringB
 
 })();
+
+
 
