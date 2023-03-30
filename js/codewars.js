1da9c520390b9aff77a6fe6c446b1e2e16966b90
Theodoric Graham <theodoricgraham@gmail.com>
2023-03-30 16:22:33 -0500

    forfeited kata solution saved

---
+++ js/codewars.js
@@ -0,0 +1,16 @@
+// function removeParentheses(str){
+//     let r = 0
+//     let x = ''
+//     for (let c of str) {
+//         if (c=='(') r++
+//         if (r==0) x+=c
+//         if (c==')') r--
+//     }
+//     console.log(x)
+//     return x
+// }
+
+
+
+
+
