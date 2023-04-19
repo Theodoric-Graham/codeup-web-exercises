5cd73c7c6098b1e7838716f5305ec72aa3fa82ba
Theodoric Graham <theodoricgraham@gmail.com>
2023-04-19 12:04:34 -0500

    finished jquery event exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,11 +1,18 @@
 $(function () {
-    $('li').css('font-size', '20px')
-    const h1 = $('h1').html()
-    $('h1').css('background-color', 'yellow')
-    $('p').css('background-color', 'yellow')
-    $('li').css('background-color', 'yellow')
-    alert(h1)
-    $('h1, p, li').css('background-color', 'yellow')
+    $('h1').click(() => {
+        $('h1').css('background-color', 'red')
+    })
+
+    $('p').dblclick(() => {
+        $('p').css('font-size', '18px')
+    })
+
+    $('li').hover(() => {
+        $('li').css('color', 'red')
+    }, () => {
+        $('li').css('color', 'black')
+
+    })
 
 });
 
