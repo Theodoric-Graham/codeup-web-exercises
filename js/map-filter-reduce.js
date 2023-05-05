2ad88dc5d3ee836b928e41ec54e965d922282a23
Theodoric Graham <theodoricgraham@gmail.com>
2023-05-05 17:03:28 -0500

    finished map filter reduce js exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,7 +41,7 @@
 
     // Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
     const filteredArr = users.filter(user => {
-          return  user.languages.length >= 3
+            return user.languages.length >= 3
         }
     )
 
@@ -50,14 +50,39 @@
 
     // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 
-    const reduceYears = users.reduce((acc, user) => {
-        console.log(users.length)
-        return acc + user.yearsOfExperience / users.length
-    }, 0)
+    const reduceYears = users.reduce((acc, user) =>
+            acc + user.yearsOfExperience / users.length
+        , 0)
+
+    // Use .reduce to get the longest email from the list of users.
+    const reduceEmailLength = users.reduce((acc, user) => {
+        if (acc.length < user.email.length) {
+            acc = user.email
+        }
+        return acc
+    }, '')
+
+    // Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+    const reduceNames = users.reduce((acc, user, i) =>
+            `${acc} ${user.name}${i === users.length - 1 ? '.' : ','} `
+        , 'Your instructors are:')
+
+    // Use .reduce to get the unique list of languages from the list of users.
+    const reduceLanguages = users.reduce((acc, user) => {
+        console.log(user.languages)
+        user.languages.forEach(el => {
+            if (!acc.includes(el)) {
+                acc += `${el} ,`
+            }
+        })
+        return acc
+    }, []);
 
 
-
-    console.log(filteredArr)
-    console.log(mapArr)
-    console.log(reduceYears)
+    // console.log(filteredArr)
+    // console.log(mapArr)
+    // console.log(reduceYears)
+    // console.log(reduceEmailLength)
+    console.log(`${reduceNames}`)
+    console.log(reduceLanguages)
 })()
